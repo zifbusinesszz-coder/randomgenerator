b7c80c43687078478fce382099e0fffbfb6362d3
zifbusinesszz-coder <zifbusinesszz@gmail.com>
2026-06-03 02:05:14 -0500

    Add configuration file with API URL and Stripe key

---
+++ config.js
@@ -0,0 +1,4 @@
+const CONFIG = {
+  API_URL: 'https://script.google.com/macros/s/AKfycbzGxYErhivjt2cevxWKlLqE6lo6UDHRI2WXk8NHCTkJf-WMEKPbdZLJXsQRMEPN3npb/exec',
+  STRIPE_PK: 'pk_live_51TdRldLeWK0oPFPo5R4P1Y6R898Xm05cYBBJm7P0tWKTdEcxJre76gYvV5aMUOZJH8YWGPr2O9S87g0rgbvhCNJu00EhneeR4m'
+};
